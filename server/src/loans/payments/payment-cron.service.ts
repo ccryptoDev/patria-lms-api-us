f5614990e92e8ace70f71066f753d2a69be51375
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-12-05 07:13:38 -0400

    updated payment cron service

--- server/src/loans/payments/payment-cron.service.ts
+++ server/src/loans/payments/payment-cron.service.ts
@@ -54,22 +54,21 @@ export class PaymentCronService {
 
     try {
       // check for payments due today
-      const paymentManagements:
-        | PaymentManagementDocument[]
-        | null = await this.paymentManagementModel.find({
-        status: {
-          $in: [
-            'in-repayment',
-            'in-repayment prime',
-            'in-repayment non-prime',
-            'in-repayment delinquent1',
-            'in-repayment delinquent2',
-            'in-repayment delinquent3',
-            'in-repayment delinquent4',
-          ],
-        },
-        canRunAutomaticPayment: true,
-      });
+      const paymentManagements: PaymentManagementDocument[] | null =
+        await this.paymentManagementModel.find({
+          status: {
+            $in: [
+              'in-repayment',
+              'in-repayment prime',
+              'in-repayment non-prime',
+              'in-repayment delinquent1',
+              'in-repayment delinquent2',
+              'in-repayment delinquent3',
+              'in-repayment delinquent4',
+            ],
+          },
+          canRunAutomaticPayment: true,
+        });
       if (!paymentManagements || paymentManagements.length <= 0) {
         this.logger.log(
           'No active loans found',
@@ -80,7 +79,8 @@ export class PaymentCronService {
 
       for (const paymentManagement of paymentManagements) {
         const user = paymentManagement.user as UserDocument;
-        const screenTracking = paymentManagement.screenTracking as ScreenTrackingDocument;
+        const screenTracking =
+          paymentManagement.screenTracking as ScreenTrackingDocument;
 
         try {
           // get default card token
@@ -107,13 +107,14 @@ export class PaymentCronService {
           }
 
           // find schedule items
-          const paymentScheduleItems: IPaymentScheduleItem[] = paymentManagement.paymentSchedule.filter(
-            (scheduleItem) =>
-              moment(scheduleItem.date)
-                .subtract(1, 'day') // allowing ACH to run end of previous day
-                .startOf('day')
-                .isSame(today) && ['opened'].includes(scheduleItem.status),
-          );
+          const paymentScheduleItems: IPaymentScheduleItem[] =
+            paymentManagement.paymentSchedule.filter(
+              (scheduleItem) =>
+                moment(scheduleItem.date)
+                  .subtract(1, 'day') // allowing ACH to run end of previous day
+                  .startOf('day')
+                  .isSame(today) && ['opened'].includes(scheduleItem.status),
+            );
           if (!paymentScheduleItems || paymentScheduleItems.length <= 0) {
             continue;
           }
@@ -131,15 +132,16 @@ export class PaymentCronService {
             );
 
             // make payment
-            const paymentAmount = paymentScheduleItem.week
-              ? paymentManagement.currentPaymentAmount
-              : paymentScheduleItem.amount;
-            const payment: PaymentDocument = await this.paymentService.makePayment(
-              paymentManagement,
-              cardToken.id,
-              paymentAmount,
-            );
-            paymentScheduleItem.payment = paymentAmount;
+            // const paymentAmount = paymentScheduleItem.week
+            //   ? paymentManagement.currentPaymentAmount
+            //   : paymentScheduleItem.amount;
+            const payment: PaymentDocument =
+              await this.paymentService.makePayment(
+                paymentManagement,
+                cardToken.id,
+                paymentScheduleItem.amount,
+              );
+            paymentScheduleItem.payment = paymentScheduleItem.amount;
             paymentScheduleItem.paidInterest = paymentScheduleItem.interest;
             paymentScheduleItem.paidPrincipal = paymentScheduleItem.principal;
             paymentScheduleItem.status = 'paid';
@@ -159,9 +161,10 @@ export class PaymentCronService {
             };
 
             // find next payment date
-            const nextPaymentScheduleItem: IPaymentScheduleItem = paymentManagement.paymentSchedule.find(
-              (scheduleItem) => scheduleItem.status === 'opened',
-            );
+            const nextPaymentScheduleItem: IPaymentScheduleItem =
+              paymentManagement.paymentSchedule.find(
+                (scheduleItem) => scheduleItem.status === 'opened',
+              );
             if (nextPaymentScheduleItem) {
               updatedPaymentManagement.nextPaymentSchedule =
                 nextPaymentScheduleItem.date;
@@ -296,11 +299,10 @@ export class PaymentCronService {
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async checkAndProcessCollectionContracts() {
-    const paymentManagements:
-      | PaymentManagementDocument[]
-      | null = await this.paymentManagementModel.find({
-      collectionsAccountStatus: 'PROMISE_TO_PAY',
-    });
+    const paymentManagements: PaymentManagementDocument[] | null =
+      await this.paymentManagementModel.find({
+        collectionsAccountStatus: 'PROMISE_TO_PAY',
+      });
     if (paymentManagements && paymentManagements.length > 0) {
       for (const paymentManagement of paymentManagements) {
         this.checkPromiseToPay(paymentManagement);
